5baa9b37d8ae8b0afc888fb7cd3e4f4da9e138cc
Angela Donati <angeladonati93@gmail.com>
2022-06-14 11:06:26 -0400

    create User repository

---
+++ src/main/java/com/technews/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.technews.repository;
+
+import com.technews.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+//repository - data access object (DAO) contains data retrieval, storage, and search capabilities
+@Repository
+//ensure repository can take User entity model and Id
+public interface UserRepository extends JpaRepository<User, Integer> {
+//    find by email query
+    User findUserByEmail(String email) throws Exception;
+}
